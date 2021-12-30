f975bad399f9bf65cc9d223682c4c0c070f6af19
Lewis Sneddon <>
2021-12-30 10:16:35 +0000

    Fixed gitignore to not exclude js files

---
+++ app/js/MainController.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+
+    /**
+     *  Here scope is listed as a string so that the minification
+     *  process won't change the variable name.
+     *  This is required since it is injected by Angular
+     */
+    angular.module('templateApp').controller('mainController', [
+        '$scope',
+        function($scope) {
+            $scope.message = 'Hello, World!';
+        }
+    ]);
+}());
---
+++ app/js/app.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+
+    /**
+     *  Changing the module name 'templateApp' requires the same change in
+     *  app/js/MainController.js line 4 and
+     *  app/index.html line 2
+     */
+    angular.module('templateApp', []);
+}());
