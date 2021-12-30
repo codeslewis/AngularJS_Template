d9e9b5ce40f04e6cef9c08b17986877f0f38d065
Lewis Sneddon <>
2021-12-30 10:05:52 +0000

    Template starter files

---
+++ scripts/web-server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var rootPath = path.normalize(__dirname + '/../');
+
+app.use(express.static(rootPath + '/app'));
+var PORT = 8000
+app.listen(PORT);
+
+console.log('Listening on PORT:', PORT);
